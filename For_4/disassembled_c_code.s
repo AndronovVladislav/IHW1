34aca09f1112197e96d1b2e3a436505ff6bf163d
BvPfy2F7cdhg3Xx <hgg0896@gmail.com>
2022-10-01 22:23:34 +0300

    v1

--- For_4/disassembled_c_code.s
+++ For_4/disassembled_c_code.s
@@ -1,39 +1,37 @@
 ; описание некоторой информации о программе, вроде
 ; названия, форматной строки для ввода-вывода и пр.
         .file   "IHW.c"
-        .intel_syntax noprefix
         .text
         .section        .rodata
 .LC0:
         .string "%d"
         .text
+        ; описание функции input()
         .globl  input
         .type   input, @function
-
-; описание функции input()
 input:
         endbr64
-        push    rbp ; пролог функции
-        mov     rbp, rsp 
-        sub     rsp, 32
-        mov     QWORD PTR -24[rbp], rdi
-        mov     DWORD PTR -28[rbp], esi
-        mov     DWORD PTR -4[rbp], 0
+        pushq   %rbp ; пролог функции
+        movq    %rsp, %rbp
+        subq    $32, %rsp
+        movq    %rdi, -24(%rbp)
+        movl    %esi, -28(%rbp)
+        movl    $0, -4(%rbp)
         jmp     .L2 ; заход в цикл
 .L3: ; тело цикла
-        mov     eax, DWORD PTR -4[rbp]
-        cdqe
-        lea     rdx, 0[0+rax*4]
-        mov     rax, QWORD PTR -24[rbp]
-        add     rax, rdx
-        mov     rsi, rax
-        lea     rdi, .LC0[rip]
-        mov     eax, 0
+        movl    -4(%rbp), %eax
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movq    %rax, %rsi
+        leaq    .LC0(%rip), %rdi
+        movl    $0, %eax
         call    __isoc99_scanf@PLT
-        add     DWORD PTR -4[rbp], 1
+        addl    $1, -4(%rbp)
 .L2: ; проверка условия
-        mov     eax, DWORD PTR -4[rbp]
-        cmp     eax, DWORD PTR -28[rbp] ; сравнение i
+        movl    -4(%rbp), %eax
+        cmpl    -28(%rbp), %eax ; сравнение i
         jl      .L3
         nop
         nop
@@ -45,76 +43,76 @@ input:
         .type   make_new_array, @function
 make_new_array:
         endbr64
-        push    rbp ; пролог функции
-        mov     rbp, rsp
-        mov     QWORD PTR -24[rbp], rdi
-        mov     QWORD PTR -32[rbp], rsi
-        mov     DWORD PTR -36[rbp], edx
-        mov     DWORD PTR -16[rbp], 0
-        mov     DWORD PTR -12[rbp], 0
+        pushq   %rbp ; пролог функции
+        movq    %rsp, %rbp
+        movq    %rdi, -24(%rbp)
+        movq    %rsi, -32(%rbp)
+        movl    %edx, -36(%rbp)
+        movl    $0, -16(%rbp)
+        movl    $0, -12(%rbp)
         jmp     .L5 ; заход в цикл
 .L7: ; тело первого цикла
-        mov     eax, DWORD PTR -12[rbp]
-        cdqe
-        lea     rdx, 0[0+rax*4]
-        mov     rax, QWORD PTR -24[rbp]
-        add     rax, rdx
-        mov     edx, DWORD PTR -16[rbp]
-        movsx   rdx, edx
-        lea     rcx, 0[0+rdx*4]
-        mov     rdx, QWORD PTR -32[rbp]
-        add     rdx, rcx
-        mov     eax, DWORD PTR [rax]
-        mov     DWORD PTR [rdx], eax
-        add     DWORD PTR -12[rbp], 2
-        add     DWORD PTR -16[rbp], 1
+        movl    -12(%rbp), %eax
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movl    -16(%rbp), %edx
+        movslq  %edx, %rdx
+        leaq    0(,%rdx,4), %rcx
+        movq    -32(%rbp), %rdx
+        addq    %rcx, %rdx
+        movl    (%rax), %eax
+        movl    %eax, (%rdx)
+        addl    $2, -12(%rbp)
+        addl    $1, -16(%rbp)
 .L5: ; проверка условий
-        mov     eax, DWORD PTR -36[rbp]
-        add     eax, 1
-        mov     edx, eax
-        shr     edx, 31
-        add     eax, edx
-        sar     eax
-        cmp     DWORD PTR -16[rbp], eax ; сравнение i
+        movl    -36(%rbp), %eax
+        addl    $1, %eax
+        movl    %eax, %edx
+        shrl    $31, %edx
+        addl    %edx, %eax
+        sarl    %eax
+        cmpl    %eax, -16(%rbp) ; сравнение i
         jge     .L6
-        mov     eax, DWORD PTR -12[rbp]
-        cmp     eax, DWORD PTR -36[rbp] ; сравнение j
+        movl    -12(%rbp), %eax
+        cmpl    -36(%rbp), %eax; сравнение j
         jl      .L7
 .L6:
-        mov     eax, DWORD PTR -36[rbp]
-        add     eax, 1
-        mov     edx, eax
-        shr     edx, 31
-        add     eax, edx
-        sar     eax
-        mov     DWORD PTR -8[rbp], eax
-        mov     DWORD PTR -4[rbp], 1
+        movl    -36(%rbp), %eax
+        addl    $1, %eax
+        movl    %eax, %edx
+        shrl    $31, %edx
+        addl    %edx, %eax
+        sarl    %eax
+        movl    %eax, -8(%rbp)
+        movl    $1, -4(%rbp)
         jmp     .L8
 .L10: ; тело второго цикла
-        mov     eax, DWORD PTR -4[rbp]
-        cdqe
-        lea     rdx, 0[0+rax*4]
-        mov     rax, QWORD PTR -24[rbp]
-        add     rax, rdx
-        mov     edx, DWORD PTR -8[rbp]
-        movsx   rdx, edx
-        lea     rcx, 0[0+rdx*4]
-        mov     rdx, QWORD PTR -32[rbp]
-        add     rdx, rcx
-        mov     eax, DWORD PTR [rax]
-        mov     DWORD PTR [rdx], eax
-        add     DWORD PTR -4[rbp], 2
-        add     DWORD PTR -8[rbp], 1
+        movl    -4(%rbp), %eax
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movl    -8(%rbp), %edx
+        movslq  %edx, %rdx
+        leaq    0(,%rdx,4), %rcx
+        movq    -32(%rbp), %rdx
+        addq    %rcx, %rdx
+        movl    (%rax), %eax
+        movl    %eax, (%rdx)
+        addl    $2, -4(%rbp)
+        addl    $1, -8(%rbp)
 .L8: ; проверка условий
-        mov     eax, DWORD PTR -8[rbp]
-        cmp     eax, DWORD PTR -36[rbp] ; сравнение i
+        movl    -8(%rbp), %eax
+        cmpl    -36(%rbp), %eax ; сравнение i
         jge     .L11
-        mov     eax, DWORD PTR -4[rbp]
-        cmp     eax, DWORD PTR -36[rbp] ; сравнение j
+        movl    -4(%rbp), %eax
+        cmpl    -36(%rbp), %eax ; сравнение j
         jl      .L10
 .L11:
         nop
-        pop     rbp ; эпилог функции
+        popq    %rbp ; эпилог функции
         ret
         .size   make_new_array, .-make_new_array
         ; описание функции output
@@ -122,39 +120,39 @@ make_new_array:
         .type   output, @function
 output:
         endbr64
-        push    rbp ; эпилог функции
-        mov     rbp, rsp
-        sub     rsp, 32
-        mov     QWORD PTR -24[rbp], rdi
-        mov     DWORD PTR -28[rbp], esi
-        mov     DWORD PTR -4[rbp], 0
+        pushq   %rbp ; эпилог функции
+        movq    %rsp, %rbp
+        subq    $32, %rsp
+        movq    %rdi, -24(%rbp)
+        movl    %esi, -28(%rbp)
+        movl    $0, -4(%rbp)
         jmp     .L13
 .L16: ; тело цикла
-        mov     eax, DWORD PTR -4[rbp]
-        cdqe
-        lea     rdx, 0[0+rax*4]
-        mov     rax, QWORD PTR -24[rbp]
-        add     rax, rdx
-        mov     eax, DWORD PTR [rax]
-        mov     esi, eax
-        lea     rdi, .LC0[rip]
-        mov     eax, 0
+        movl    -4(%rbp), %eax
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movl    (%rax), %eax
+        movl    %eax, %esi
+        leaq    .LC0(%rip), %rdi
+        movl    $0, %eax
         call    printf@PLT
-        mov     eax, DWORD PTR -28[rbp]
-        sub     eax, 1
-        cmp     DWORD PTR -4[rbp], eax
+        movl    -28(%rbp), %eax
+        subl    $1, %eax
+        cmpl    %eax, -4(%rbp)
         je      .L14
-        mov     edi, 32
+        movl    $32, %edi
         call    putchar@PLT
         jmp     .L15
 .L14:
-        mov     edi, 10
+        movl    $10, %edi
         call    putchar@PLT
 .L15:
-        add     DWORD PTR -4[rbp], 1
+        addl    $1, -4(%rbp)
 .L13: ; проверка условия
-        mov     eax, DWORD PTR -4[rbp]
-        cmp     eax, DWORD PTR -28[rbp] ; сравнение i
+        movl    -4(%rbp), %eax
+        cmpl    -28(%rbp), %eax; сравнение i
         jl      .L16
         nop
         nop
@@ -166,152 +164,152 @@ output:
         .type   main, @function
 main:
         endbr64
-        push    rbp ; пролог функции
-        mov     rbp, rsp
-        push    r15
-        push    r14
-        push    r13
-        push    r12
-        push    rbx
-        sub     rsp, 88
-        mov     rax, QWORD PTR fs:40
-        mov     QWORD PTR -56[rbp], rax
-        xor     eax, eax
-        mov     rax, rsp
-        mov     rbx, rax
-        lea     rax, -92[rbp]
-        mov     rsi, rax
-        lea     rdi, .LC0[rip]
-        mov     eax, 0
+        pushq   %rbp ; пролог функции
+        movq    %rsp, %rbp
+        pushq   %r15
+        pushq   %r14
+        pushq   %r13
+        pushq   %r12
+        pushq   %rbx
+        subq    $88, %rsp
+        movq    %fs:40, %rax
+        movq    %rax, -56(%rbp)
+        xorl    %eax, %eax
+        movq    %rsp, %rax
+        movq    %rax, %rbx
+        leaq    -92(%rbp), %rax
+        movq    %rax, %rsi
+        leaq    .LC0(%rip), %rdi
+        movl    $0, %eax
         call    __isoc99_scanf@PLT ; считывание n
-        mov     eax, DWORD PTR -92[rbp]
-        movsx   rdx, eax
-        sub     rdx, 1
-        mov     QWORD PTR -88[rbp], rdx
-        movsx   rdx, eax
-        mov     QWORD PTR -112[rbp], rdx
-        mov     QWORD PTR -104[rbp], 0
-        movsx   rdx, eax
-        mov     QWORD PTR -128[rbp], rdx
-        mov     QWORD PTR -120[rbp], 0
-        cdqe
-        lea     rdx, 0[0+rax*4]
-        mov     eax, 16
-        sub     rax, 1
-        add     rax, rdx
-        mov     edi, 16
-        mov     edx, 0
-        div     rdi
-        imul    rax, rax, 16
-        mov     rdx, rax
-        and     rdx, -4096
-        mov     rcx, rsp
-        sub     rcx, rdx
-        mov     rdx, rcx
+        movl    -92(%rbp), %eax
+        movslq  %eax, %rdx
+        subq    $1, %rdx
+        movq    %rdx, -88(%rbp)
+        movslq  %eax, %rdx
+        movq    %rdx, -112(%rbp)
+        movq    $0, -104(%rbp)
+        movslq  %eax, %rdx
+        movq    %rdx, -128(%rbp)
+        movq    $0, -120(%rbp)
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movl    $16, %eax
+        subq    $1, %rax
+        addq    %rdx, %rax
+        movl    $16, %edi
+        movl    $0, %edx
+        divq    %rdi
+        imulq   $16, %rax, %rax
+        movq    %rax, %rdx
+        andq    $-4096, %rdx
+        movq    %rsp, %rcx
+        subq    %rdx, %rcx
+        movq    %rcx, %rdx
 .L18:
-        cmp     rsp, rdx
+        cmpq    %rdx, %rsp
         je      .L19
-        sub     rsp, 4096
-        or      QWORD PTR 4088[rsp], 0
+        subq    $4096, %rsp
+        orq     $0, 4088(%rsp)
         jmp     .L18
 .L19:
-        mov     rdx, rax
-        and     edx, 4095
-        sub     rsp, rdx
-        mov     rdx, rax
-        and     edx, 4095
-        test    rdx, rdx
+        movq    %rax, %rdx
+        andl    $4095, %edx
+        subq    %rdx, %rsp
+        movq    %rax, %rdx
+        andl    $4095, %edx
+        testq   %rdx, %rdx
         je      .L20
-        and     eax, 4095
-        sub     rax, 8
-        add     rax, rsp
-        or      QWORD PTR [rax], 0
+        andl    $4095, %eax
+        subq    $8, %rax
+        addq    %rsp, %rax
+        orq     $0, (%rax)
 .L20:
-        mov     rax, rsp
-        add     rax, 3
-        shr     rax, 2
-        sal     rax, 2
-        mov     QWORD PTR -80[rbp], rax
-        mov     eax, DWORD PTR -92[rbp]
-        movsx   rdx, eax
-        sub     rdx, 1
-        mov     QWORD PTR -72[rbp], rdx
-        movsx   rdx, eax
-        mov     r14, rdx
-        mov     r15d, 0
-        movsx   rdx, eax
-        mov     r12, rdx
-        mov     r13d, 0
-        cdqe
-        lea     rdx, 0[0+rax*4]
-        mov     eax, 16
-        sub     rax, 1
-        add     rax, rdx
-        mov     ecx, 16
-        mov     edx, 0
-        div     rcx
-        imul    rax, rax, 16
-        mov     rdx, rax
-        and     rdx, -4096
-        mov     rsi, rsp
-        sub     rsi, rdx
-        mov     rdx, rsi
+        movq    %rsp, %rax
+        addq    $3, %rax
+        shrq    $2, %rax
+        salq    $2, %rax
+        movq    %rax, -80(%rbp)
+        movl    -92(%rbp), %eax
+        movslq  %eax, %rdx
+        subq    $1, %rdx
+        movq    %rdx, -72(%rbp)
+        movslq  %eax, %rdx
+        movq    %rdx, %r14
+        movl    $0, %r15d
+        movslq  %eax, %rdx
+        movq    %rdx, %r12
+        movl    $0, %r13d
+        cltq
+        leaq    0(,%rax,4), %rdx
+        movl    $16, %eax
+        subq    $1, %rax
+        addq    %rdx, %rax
+        movl    $16, %ecx
+        movl    $0, %edx
+        divq    %rcx
+        imulq   $16, %rax, %rax
+        movq    %rax, %rdx
+        andq    $-4096, %rdx
+        movq    %rsp, %rsi
+        subq    %rdx, %rsi
+        movq    %rsi, %rdx
 .L21:
-        cmp     rsp, rdx
+        cmpq    %rdx, %rsp
         je      .L22
-        sub     rsp, 4096
-        or      QWORD PTR 4088[rsp], 0
+        subq    $4096, %rsp
+        orq     $0, 4088(%rsp)
         jmp     .L21
 .L22:
-        mov     rdx, rax
-        and     edx, 4095
-        sub     rsp, rdx
-        mov     rdx, rax
-        and     edx, 4095
-        test    rdx, rdx
+        movq    %rax, %rdx
+        andl    $4095, %edx
+        subq    %rdx, %rsp
+        movq    %rax, %rdx
+        andl    $4095, %edx
+        testq   %rdx, %rdx
         je      .L23
-        and     eax, 4095
-        sub     rax, 8
-        add     rax, rsp
-        or      QWORD PTR [rax], 0
-.L23: 
+        andl    $4095, %eax
+        subq    $8, %rax
+        addq    %rsp, %rax
+        orq     $0, (%rax)
+.L23:
         ; вызов самописных функций, выше - 
         ; служебное, scanf и создание массивов
-        mov     rax, rsp
-        add     rax, 3
-        shr     rax, 2
-        sal     rax, 2
-        mov     QWORD PTR -64[rbp], rax
-        mov     edx, DWORD PTR -92[rbp]
-        mov     rax, QWORD PTR -80[rbp]
-        mov     esi, edx
-        mov     rdi, rax
+        movq    %rsp, %rax
+        addq    $3, %rax
+        shrq    $2, %rax
+        salq    $2, %rax
+        movq    %rax, -64(%rbp)
+        movl    -92(%rbp), %edx
+        movq    -80(%rbp), %rax
+        movl    %edx, %esi
+        movq    %rax, %rdi
         call    input
-        mov     edx, DWORD PTR -92[rbp]
-        mov     rcx, QWORD PTR -64[rbp]
-        mov     rax, QWORD PTR -80[rbp]
-        mov     rsi, rcx
-        mov     rdi, rax
+        movl    -92(%rbp), %edx
+        movq    -64(%rbp), %rcx
+        movq    -80(%rbp), %rax
+        movq    %rcx, %rsi
+        movq    %rax, %rdi
         call    make_new_array
-        mov     edx, DWORD PTR -92[rbp]
-        mov     rax, QWORD PTR -64[rbp]
-        mov     esi, edx
-        mov     rdi, rax
+        movl    -92(%rbp), %edx
+        movq    -64(%rbp), %rax
+        movl    %edx, %esi
+        movq    %rax, %rdi
         call    output
-        mov     eax, 0
-        mov     rsp, rbx
-        mov     rbx, QWORD PTR -56[rbp]
-        xor     rbx, QWORD PTR fs:40
+        movl    $0, %eax
+        movq    %rbx, %rsp
+        movq    -56(%rbp), %rbx
+        xorq    %fs:40, %rbx
         je      .L25
         call    __stack_chk_fail@PLT
 .L25:
-        lea     rsp, -40[rbp]
-        pop     rbx ; эпилог функции
-        pop     r12
-        pop     r13
-        pop     r14
-        pop     r15
-        pop     rbp
+        leaq    -40(%rbp), %rsp ; эпилог функции
+        popq    %rbx
+        popq    %r12
+        popq    %r13
+        popq    %r14
+        popq    %r15
+        popq    %rbp
         ret
         .size   main, .-main
         .ident  "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
